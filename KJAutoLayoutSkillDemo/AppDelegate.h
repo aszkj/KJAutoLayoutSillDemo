aabc2ad5d9c0c2684d4834c44d00720e050c60a3
mm <mm@mmdemac-mini.local>
2017-04-18 10:41:48 -0700

    Initial Commit

---
+++ KJAutoLayoutSkillDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  KJAutoLayoutSkillDemo
+//
+//  Created by mm on 17/4/18.
+//  Copyright © 2017年 mm. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ KJAutoLayoutSkillDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  KJAutoLayoutSkillDemo
+//
+//  Created by mm on 17/4/18.
+//  Copyright © 2017年 mm. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
